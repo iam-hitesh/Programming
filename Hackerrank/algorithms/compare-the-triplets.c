ffe4b2541add53b6cf80fa341c4b6855ab8256fe
Hitesh Yadav <hhiteshyadav@gmail.com>
2017-09-07 17:45:59 +0530

    Create compare-the-triplets.c

---
+++ Hackerrank/algorithms/compare-the-triplets.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int* solve(int a0, int a1, int a2, int b0, int b1, int b2, int *result_size){
+    // Complete this function
+}
+
+int main() {
+    int a0; 
+    int a1; 
+    int a2; 
+    scanf("%d %d %d", &a0, &a1, &a2);
+    int b0; 
+    int b1; 
+    int b2; 
+    scanf("%d %d %d", &b0, &b1, &b2);
+    int result_size;
+    int* result = solve(a0, a1, a2, b0, b1, b2, &result_size);
+    for(int result_i = 0; result_i < result_size; result_i++) {
+        if(result_i) {
+            printf(" ");
+        }
+        printf("%d", result[result_i]);
+    }
+    puts("");
+    
+
+    return 0;
+}
