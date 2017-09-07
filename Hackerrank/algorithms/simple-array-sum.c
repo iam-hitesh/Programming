55a5fa567bdc3defc4efcc7bcfecefed7c61a035
Hitesh Yadav <hhiteshyadav@gmail.com>
2017-09-07 17:56:14 +0530

    Create simple-array-sum.c

---
+++ Hackerrank/algorithms/simple-array-sum.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int simpleArraySum(int n, int ar_size, int* ar) {
+    // Complete this function
+}
+
+int main() {
+    int n; 
+    scanf("%i", &n);
+    int *ar = malloc(sizeof(int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       scanf("%i",&ar[ar_i]);
+    }
+    int result = simpleArraySum(n, n, ar);
+    printf("%d\n", result);
+    return 0;
+}
