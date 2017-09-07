9eb07342f0ec6671e26e83f3d0e96f1631b04c00
Hitesh Yadav <hhiteshyadav@gmail.com>
2017-09-07 17:53:20 +0530

    Create staircase.c

---
+++ Hackerrank/algorithms/staircase.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n,i,j,num; 
+    scanf("%d",&n);
+    for(int j=0;j<n;j++){
+            for(int i=1;i<=n;i++){
+                if(i < n - j){
+                    printf(" ");
+                }
+                else{
+                    printf("#");
+                }
+            }
+            printf("\n");
+        }
+}
