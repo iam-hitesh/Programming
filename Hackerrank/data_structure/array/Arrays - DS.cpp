3d92fa48e4546f9de9bdd4dabf5a80c067d72dd9
Hitesh Yadav <hhiteshyadav@gmail.com>
2018-02-15 15:25:13 +0530

    Create Arrays - DS.cpp

---
+++ Hackerrank/data_structure/array/Arrays - DS.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    for (int arr_i = n - 1;arr_i >= 0;arr_i--){
+        cout<<arr[arr_i]<<" ";
+    }
+    return 0;
+}
