f32769c4bbd9f8c02b122345366d1f5b7ce34905
Hitesh Yadav <hhiteshyadav@gmail.com>
2018-02-19 12:04:24 +0530

    Create Inserting a Node Into a Sorted Doubly Linked List.cpp

---
+++ Hackerrank/data_structure/linked_list/Inserting a Node Into a Sorted Doubly Linked List.cpp
@@ -0,0 +1,62 @@
+Node* SortedInsert(Node *head,int data)
+{
+    // Complete this function
+   // Do not write the main method. 
+    
+    struct Node *new_node = new Node();
+    struct Node* temp = head;
+    new_node->data = data;
+    
+    //case when List is empty
+    if(head==NULL) {
+        
+        head = new_node;
+        return head;
+        
+    }
+    
+    //insertion at beginning of List
+    else if(data < temp->data) {
+        new_node->next = temp;
+        temp->prev = new_node;
+        head =  new_node;
+        return head;
+    }
+    
+    //walking with 2 pointers
+    else{
+        
+        struct Node* temp1 = NULL;
+        
+        //traversing only if new_node has larger data
+        while(temp!= NULL && temp->data < data) {
+            temp1 = temp;//temp1 is node previous to temp
+            temp=temp->next;
+        }
+        
+        //insertion at end of LL
+        if(temp==NULL) {
+            temp1->next = new_node;
+            new_node->prev = temp1;
+            new_node->next = NULL;
+        }
+        
+        //insertion in between
+        //temp1 is node prev to temp
+        //temp is ahead of temp1
+        //new_node has to be inserted between temp and temp1
+        else{
+            temp1->next = new_node;
+            temp->prev = new_node;
+            
+            new_node->next = temp;
+            new_node->prev = temp1;
+        }
+            
+            
+        
+        return head;
+        
+    }
+    
+}
