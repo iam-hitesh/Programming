17ddccb4b9cb2777138990d2f3c8a4dd4869fa0d
Hitesh Yadav <hhiteshyadav@gmail.com>
2018-02-20 10:07:52 +0530

    Create string_input.cpp

---
+++ data_structure/string_input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include<cstring>
+
+using namespace std;
+
+int main(){
+  string exp;
+  cout<<"Enter a String";
+  // cin>>exp;
+  // cout<<exp;
+  getline(cin,exp);
+  cout<<exp;
+}
