cc79763b501ccfc3e0398a93cafea0526458731a
Hitesh Yadav <hhiteshyadav@gmail.com>
2018-02-21 10:29:42 +0530

    Create balanced_brackets.cpp

---
+++ Hackerrank/data_structure/Stack/balanced_brackets.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+bool isCheck(char opening,char closing){
+    if(opening == '{' && closing == '}') return true;
+    else if(opening == '(' && closing == ')') return true;
+    else if(opening == '[' && closing == ']') return true;
+    else return false;
+}
+
+bool isBalanced(string exp){
+    stack<char> S;
+    for(int i = 0;i<exp.length();i++){
+        if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{'){
+            S.push(exp[i]);
+        }else if(exp[i] == ')' || exp[i] == ']' || exp[i] == '}'){
+            if(S.empty() || !isCheck(S.top(),exp[i])){
+                return false;
+        }else{
+                S.pop();
+            }
+        }
+    }
+    return S.empty() ? true:false;
+}
+
+int main(){
+    int test;
+    cin>>test;
+    for(int i = 0;i<test;i++){
+        string exp;
+        cin>>exp;
+        if(isBalanced(exp)){
+            cout<<"YES \n";
+        }else{
+            cout<<"NO \n";
+        }
+    }
+}
